0dc5db44ac35a6c0dc91fd38b18a73fe99c17383
Jorge Cossi <jorge_cossi@yahoo.com>
2016-11-16 21:30:47 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+
+
+y=5
+while y>0
+  if y % 2 == 0 then
+    puts "-----------"
+  else
+    puts "   |   |   "
+  end
+  y=y-1
+end
+end
