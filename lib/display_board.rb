595b4d26b36322a30fb46b43936d7cd59535143a
Marcus Havanek <marcus.havranek@havranek.co.at>
2016-12-17 10:34:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  fields = "   |   |   "
+  separator = "-----------"
+
+  puts fields
+  puts separator
+  puts fields
+  puts separator
+  puts fields
+end
