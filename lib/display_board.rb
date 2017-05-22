8ded0cd7fabff554baca2a61ba80bc5a98e3304f
Uzor Jon-Ubabuco <uubabukoh@gmail.com>
2017-05-22 19:53:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  board = []
+  for i in 0..8
+    board[i] = " "
+  end
+
+  result = []
+  result[1] = result[3] = "-----------"
+  result[0] = " #{board[0]} | #{board[1]} | #{board[2]} "
+  result[2] = " #{board[3]} | #{board[4]} | #{board[5]} "
+  result[4] = " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts result
+end
+
+display_board
