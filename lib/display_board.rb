2d9909c2d6dfd8d89086397d49101d882d172a42
Brian Schoonmaker <brian.a.schoonmaker@gmail.com>
2017-09-12 23:12:20 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,17 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell = "   "
+  @board = Array.new(3) { Array.new(3, cell) }
+  @rowNum = 1
+  @board.to_a.each do |row|
+      row1 = row[0] + "|" + row[1] + "|" + row[2]
+      puts row1
+      if(@rowNum == 3)
+        break
+      else
+        @rowNum += 1
+        puts "-----------"
+      end
+  end
+end
+
+display_board
